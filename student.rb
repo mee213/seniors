04a0eb5010ef6147b6badc838121941787a07a75
mee213 <evans.marlo@gmail.com>
2013-11-04 12:39:26 -0600

    Added Juniors

--- student.rb
+++ student.rb
@@ -7,6 +7,10 @@ class Student
 		@grade = grade
 	end
 
+	def junior?
+		grade == 11
+	end
+
 	def senior?
 		grade == 12
 	end
@@ -14,6 +18,10 @@ class Student
 	def to_s
 		"#{last_name}, #{first_name}"
 	end
+end
+
+def juniors(students)
+	students.select { |student| student.junior? }
 end
 
 def seniors(students)
@@ -25,6 +33,16 @@ sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
+puts "Seniors"
+
 seniors(all_students).each do |student|
-	puts student
+	puts " * " + student.to_s
+end
+
+puts
+
+puts "Juniors"
+
+juniors(all_students).each do |student|
+	puts " * " + student.to_s
 end
